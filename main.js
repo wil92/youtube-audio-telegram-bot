c6c12276ed41a2d327cc86734ff27cc0d4bce9ad
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-12-11 12:11:02 +0100

    Add support for messages

--- main.js
+++ main.js
@@ -20,22 +20,32 @@ function removeAudio(audioName) {
     fs.unlinkSync(audioName);
 }
 
+function getMessageText(elem) {
+    return elem.channel_post ? elem.channel_post.text : elem.message.text;
+}
+
+function getChatId(elem) {
+    return elem.channel_post ? elem.channel_post.chat.id : elem.message.chat.id;
+}
+
 function downloadYoutubeVideo(elem, options = '') {
     const { exec } = require("child_process");
     return new Promise((resolve, reject) => {
-        exec(`youtube-dl ${options} ${elem.channel_post.text}`, (error, stdout, stderr) => {
+        exec(`youtube-dl ${options} ${getMessageText(elem)}`, (error, stdout, stderr) => {
             if (error) {
-                console.log(`error: ${error.message}`);
                 return reject(error);
             }
             if (stderr) {
-                console.log(`stderr: ${stderr}`);
                 return reject(stderr);
             }
-            console.log(`stdout: ${stdout}`);
             resolve(stdout);
         });
     });
+}
+
+function checkYoutubeLink(elem) {
+    return (elem.channel_post && isYoutubeLink(elem.channel_post.text)) ||
+        (elem.message && isYoutubeLink(elem.message.text));
 }
 
 function process () {
@@ -44,13 +54,13 @@ function process () {
             const tmpMarkedPost = new Set();
             res.result.forEach(elem => {
                 tmpMarkedPost.add(elem.update_id);
-                if (elem.channel_post && isYoutubeLink(elem.channel_post.text) && !markedPost.has(elem.update_id)) {
+                if (checkYoutubeLink(elem) && !markedPost.has(elem.update_id)) {
                     downloadYoutubeVideo(elem, "-x --audio-format mp3 --audio-quality 7 -q")
                         .then(() => downloadYoutubeVideo(elem, "--get-filename"))
                         .then(fileName => {
                             fileName = toMP3(fileName);
                             console.log(fileName);
-                            return uploadAudio(fileName, elem.channel_post.chat.id).then(() => fileName);
+                            return uploadAudio(fileName, getChatId(elem)).then(() => fileName);
                         })
                         .then(fileName => removeAudio(`${__dirname}/${fileName}`));
                 }
@@ -64,7 +74,7 @@ function process () {
 getUpdates().then(res => {
     if (Array.isArray(res.result)) {
         res.result.forEach(elem => {
-            if (elem.channel_post && isYoutubeLink(elem.channel_post.text)) {
+            if (checkYoutubeLink(elem)) {
                 markedPost.add(elem.update_id);
             }
         })
